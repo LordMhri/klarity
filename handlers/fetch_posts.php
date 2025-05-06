a9027a4cf1da89a4fad949bd6bb41bb1929601f3
lordmhri <meheret.alemu-ug@aau.edu.et>
2025-05-06 18:11:06 +0300

    fetching posts done, .env configured and config added

---
+++ handlers/fetch_posts.php
@@ -0,0 +1,18 @@
+<?php
+
+//TODO : ensure cookies are valid before giving questions
+require_once  "config.php";
+header('Content-Type: application/json');
+$conn = new_PDO_connection();
+
+
+$fetch_posts = "SELECT * from posts order by created_at desc";
+
+try {
+    $stmt = $conn->prepare($fetch_posts);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($result);
+} catch (PDOException $e){
+    echo "Error: " . $e->getMessage();
+}
--- public/post/create_post.php
+++ public/post/create_post.php
@@ -1,3 +1,10 @@
+<?php
+//session_start();
+//if (!$_SESSION['username']) {
+//    header("Location: ../auth.html");
+//    exit();
+//}
+//?>
 <!DOCTYPE html>
 <html>
 <head>
