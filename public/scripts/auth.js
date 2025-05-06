594a5834117f9e97f0531b1dde26b90b75740341
lordmhri <meheret.alemu-ug@aau.edu.et>
2025-05-06 20:29:42 +0300

    Added proper toggling of login and register button

---
+++ public/scripts/auth.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginBtn = document.getElementById('loginToggle');
+    const signUpBtn = document.getElementById('signupToggle');
+    const loginForm = document.getElementById('loginForm');
+    const signupForm = document.getElementById('signupForm');
+
+    loginBtn.addEventListener('click', () => {
+        loginForm.style.display = 'block';
+        signupForm.style.display = 'none';
+
+        signUpBtn.classList.remove('active');
+        loginBtn.classList.add('active');
+    })
+
+    signUpBtn.addEventListener('click', () => {
+        loginForm.style.display = 'none';
+        signupForm.style.display = 'block';
+        signUpBtn.classList.add('active');
+        loginBtn.classList.remove('active');
+    })
+
+});
